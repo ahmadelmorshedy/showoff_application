e3889a68a28c050a9f1e483442d000c8470ea2ca
ahmadelmorshedy <ah.morshedy@gmail.com>
2018-02-23 06:31:25 +0200

    Adding Exception Handler

--- app/controllers/instagram_controller.rb
+++ app/controllers/instagram_controller.rb
@@ -2,9 +2,14 @@ class InstagramController < ApplicationController
   before_action :require_login, only: :recent_media
   
   def callback
-    instagram_api = InstagramServices::InstagramApiService.instagram_api_service(code: params[:code])
-    session[:access_token] = instagram_api.access_token
-    redirect_to action: "recent_media"
+    begin
+      instagram_api = InstagramServices::InstagramApiService.instagram_api_service(code: params[:code])
+      session[:access_token] = instagram_api.access_token
+      redirect_to root_url, alert: I18n.t('flashes.error') if session[:access_token].nil?
+      redirect_to action: "recent_media"
+    rescue StandardError => error
+      redirect_to root_url, alert: I18n.t('flashes.error')
+    end
   end
 
   def recent_media
